1a2304cfc7ac60e4ae6fa810c360387f61a7a7c6
donRumata <donrumata03@gmail.com>
2020-11-01 14:47:07 +0300

    modernize testing

--- math_bot_config.h
+++ math_bot_config.h
@@ -9,6 +9,8 @@
 
 
 constexpr const char* math_bot_pch_file_path = __FILE__;
-inline std::filesystem::path math_bot_base_dir = get_path_folder(math_bot_pch_file_path);
+inline const std::filesystem::path math_bot_base_dir = get_path_folder(math_bot_pch_file_path);
 
+inline const auto math_bot_query_path = math_bot_base_dir / ".." / "queries";
+inline const auto math_bot_result_path = math_bot_base_dir / ".." / "results";
 
