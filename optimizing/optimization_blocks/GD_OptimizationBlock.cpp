b2ca4ae6d570ca4a6e31881d7368de47986f6120
donRumata <donrumata03@gmail.com>
2021-07-22 09:44:51 +0300

    Generate GD optimizer methods

--- optimizing/optimization_blocks/GD_OptimizationBlock.cpp
+++ optimizing/optimization_blocks/GD_OptimizationBlock.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "GD_OptimizationBlock.h"
+
+OptimizationBlock::type GD_OptimizationBlock::get_type ()
+{
+	return type::GD;
+}
+
+void GD_OptimizationBlock::update_optimization_objective (
+		const std::function<double (const std::vector<double>&)>& _error_function,
+		const std::function<double (const std::vector<double>&)>& _fitness_function,
+		const std::function<std::vector<double> (const std::vector<double>&)>& _first_gradient,
+		const std::function<std::vector<double> (const std::vector<double>&)>& _second_gradient,
+		const std::vector<double>& gradient_tree_sizes)
+{
+
+}
+
+void GD_OptimizationBlock::run (const std::vector<double>& parent_genome,
+                                const std::vector<std::pair<double, double>>& point_ranges)
+{
+
+}
+
+std::pair<double, std::vector<double>> GD_OptimizationBlock::get_result ()
+{
+	return std::pair<double, std::vector<double>>();
+}
+
+double GD_OptimizationBlock::iteration_cost_units ()
+{
+	return OptimizationBlock::iteration_cost_units();
+}
--- optimizing/optimization_blocks/GD_OptimizationBlock.h
+++ optimizing/optimization_blocks/GD_OptimizationBlock.h
@@ -9,7 +9,21 @@
 
 class GD_OptimizationBlock : OptimizationBlock
 {
+	type get_type () override;
 
+	void update_optimization_objective (const std::function<double (const std::vector<double>&)>& _error_function,
+	                                    const std::function<double (const std::vector<double>&)>& _fitness_function,
+	                                    const std::function<std::vector<double> (
+			                                    const std::vector<double>&)>& _first_gradient,
+	                                    const std::function<std::vector<double> (
+			                                    const std::vector<double>&)>& _second_gradient,
+	                                    const std::vector<double>& gradient_tree_sizes) override;
+
+	void run (const std::vector<double>& parent_genome, const std::vector<std::pair<double, double>>& point_ranges) override;
+
+	double iteration_cost_units () override;
+
+	std::pair<double, std::vector<double>> get_result () override;
 };
 
 
