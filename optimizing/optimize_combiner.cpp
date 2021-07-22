64cb6fdeda9fee6d88f3a1969bd4274f8f9889e7
donRumata <donrumata03@gmail.com>
2021-07-22 13:59:46 +0300

    Add informing about genome after container iteration

--- optimizing/optimize_combiner.cpp
+++ optimizing/optimize_combiner.cpp
@@ -129,6 +129,17 @@ void OptimizationTree::run (const std::function<double (const std::vector<double
 		std::cout << console_colors::underlined << "Start " << (m_type == type::seq_container ? "sequence" : "parallel")
 		<< " container" << console_colors::remove_all_colors << " {" << std::endl;
 
+		auto inform_about_best_genome = [&] (const std::string& container_name) {
+			std::cout << "[" + container_name + "]: best genome is";
+			if (best_sequence.size() <= 7) {
+				std::cout << ": " << best_sequence;
+			}
+			else {
+				std::cout << " too long to print";
+			}
+			std::cout << std::endl;
+		};
+
 		if (m_type == type::seq_container) {
 			/// Run children in sequence, choose best of children results
 			auto current_optimal_result = parent_result;
@@ -148,6 +159,8 @@ void OptimizationTree::run (const std::function<double (const std::vector<double
 				}
 				std::cout << "[SeqContainer]: best_error after block „" << child.m_block->get_type_name() << "”: " << current_optimal_result->first
 				          << "(" << (opt_updated_now ? "updated" : "not updated") << ")" << std::endl;
+
+				inform_about_best_genome("SeqContainer");
 
 				new_blocks_with_connections.emplace_back(child.m_block->get_type_name());
 			}
@@ -172,6 +185,8 @@ void OptimizationTree::run (const std::function<double (const std::vector<double
 				}
 				std::cout << "[ParContainer]: next block processed (" << child.m_block->get_type_name() << "): best_error is "
 				<< best_error << "(" << (renewed_now ? "updated" : "not updated") << ")" << std::endl;
+
+				inform_about_best_genome("SeqContainer");
 			}
 		}
 
