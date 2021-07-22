625e01ec06bbd1eea1e43357b31ced2db8498a4f
donRumata <donrumata03@gmail.com>
2021-07-22 16:01:58 +0300

    Add generated lambdas for error, 1st and 2nd gradients and fitness in optimization function

--- optimizing/OptimizationTreeWrapper.cpp
+++ optimizing/OptimizationTreeWrapper.cpp
@@ -53,6 +53,66 @@ std::pair<std::unordered_map<std::string, double>, double> OptimizationTreeWrapp
 	printvar(first_derivative_tree_nodes);
 	printvar(second_derivative_tree_nodes);
 
+	/// 							Backwards converter (sequence --> umap):
+	auto convert_variable_sequence = [&](const std::vector<double>& variable_values) -> std::unordered_map<std::string, double>
+	{
+		std::unordered_map<std::string, double> res;
+		for (size_t var_index = 0; var_index < n_vars; var_index++) res[var_seq[var_index]] = variable_values[var_index];
+
+		return res;
+	};
+
+
+
+	/// 								Sequence ->> Error (ℝ^n → ℝ)
+	auto generated_error_function = [&](const std::vector<double>& variable_values) -> double
+	{
+		std::unordered_map < std::string, double > vars = convert_variable_sequence(variable_values);
+
+		try {
+			return tree->compute(vars);
+		}
+		catch (std::exception& e)
+		{
+			std::cout << console_colors::red << "[OptimizationTreeWrapper ->> GeneratedErrorFunction] Error caught: " << e.what() << std::endl;
+			throw;
+		}
+	};
+
+	///												Gradient counter generator
+	auto generate_some_gradient_counter = [&convert_variable_sequence]
+			(const std::vector<std::unique_ptr<expression_tree>>& derivative_trees)
+	{
+		return [&convert_variable_sequence, &derivative_trees] (const std::vector<double>& variable_values) -> std::vector<double>
+		{
+			std::vector<double> res(variable_values.size());
+			std::unordered_map < std::string, double > vars = convert_variable_sequence(variable_values);
+
+			for (size_t dim_number = 0; dim_number < variable_values.size(); ++dim_number) {
+				res[dim_number] = derivative_trees[dim_number]->compute(vars);
+			}
+
+			return res;
+		};
+	};
+
+
+	/// 								Sequence ->> First gradient (ℝ^n → ℝ^n)
+	auto generated_first_gradient = generate_some_gradient_counter(first_derivative_trees);
+
+	/// 								Sequence ->> Second gradient (ℝ^n → ℝ^n)
+	auto generated_second_gradient = generate_some_gradient_counter(second_derivative_trees);
+
+	/// 								Sequence ->> Fitness (ℝ^n → ℝ)
+	auto generated_fitness_function = [&](const std::vector<double>& variable_values)
+	{
+		double function_result = generated_error_function(variable_values);
+
+		if (function_result - target_minimum < 1e-10) return 2e+15;
+
+		double fitness = 1 / std::abs(function_result - target_minimum);
+		return fitness;
+	};
 
 
 
