880c3e237f78d7cde260f8b260c5d20887eb4dd9
donRumata03 <donrumata03@gmail.com>
2021-07-24 22:24:41 +0300

    Use std:: function for getting parent paths

--- math_bot_config.h
+++ math_bot_config.h
@@ -13,6 +13,6 @@ constexpr const char* math_bot_pch_file_path = __FILE__;
 inline std::filesystem::path math_bot_base_dir = safe_get_parent(std::string{math_bot_pch_file_path});
 		// get_path_folder(math_bot_pch_file_path);
 
-inline const auto math_bot_query_path = math_bot_base_dir / ".." / "queries";
-inline const auto math_bot_result_path = math_bot_base_dir / ".." / "results";
+inline const auto math_bot_query_path = math_bot_base_dir.parent_path() / "queries";
+inline const auto math_bot_result_path = math_bot_base_dir.parent_path() / "results";
 
